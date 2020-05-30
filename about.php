1d94ab7f07daa9c94a1ce1dae2c682c5ec0c44b6
amirbagh75 <amirbagh75@gmail.com>
2020-05-30 15:47:27 +0430

    🐛 fix structure

--- about.php
+++ about.php
@@ -1,4 +1,4 @@
 <?php
 session_start();
 
-echo "Home page.";
+echo "About page.";
--- config/db.php
+++ config/db.php
@@ -2,13 +2,23 @@
 require 'vendor/autoload.php';
 
 use Medoo\Medoo;
- 
-$database = new Medoo([
-    'database_type' => 'mysql',
-    'database_name' => getenv('DB_NAME'),
-    'port' => getenv('DB_PORT'),
-    'server' => getenv('DB_HOST'),
-    'username' => getenv('DB_USER'),
-    'password' => getenv('DB_PASS'),
-    'charset' => 'utf8'
-]);
+
+if (
+    trim(getenv('DB_HOST')) !== "" &&
+    trim(getenv('DB_NAME')) !== "" &&
+    trim(getenv('DB_USER')) !== "" &&
+    trim(getenv('DB_PASS')) !== ""
+) {
+    $database = new Medoo([
+        'database_type' => 'mysql',
+        'database_name' => getenv('DB_NAME'),
+        'port' => getenv('DB_PORT'),
+        'server' => getenv('DB_HOST'),
+        'username' => getenv('DB_USER'),
+        'password' => getenv('DB_PASS'),
+        'charset' => 'utf8'
+    ]);
+} else {
+    echo "Exited. Please add DB ENVs.";
+    exit(0);
+}
