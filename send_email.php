f1ab48febd8bade447b8abde0668f84d3c97b950
Ali Najmabadi <alinajmabadizadeh2002@gmail.com>
2024-09-29 09:53:48 +0330

    update email-server

---
+++ send_email.php
@@ -0,0 +1,42 @@
+<?php
+require 'vendor/autoload.php'; // Load Composer dependencies
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+use Dotenv\Dotenv;
+
+// Load environment variables from .env file
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+// Create an instance of PHPMailer
+$mail = new PHPMailer(true);
+
+try {
+    // Server settings
+    $mail->isSMTP();
+    $mail->Host       = $_ENV['MAIL_HOST'];
+    $mail->SMTPAuth   = true;
+    $mail->Username   = $_ENV['MAIL_USER'];
+    $mail->Password   = $_ENV['MAIL_PASSWORD'];
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS; // Use STARTTLS encryption
+    $mail->Port       = $_ENV['MAIL_PORT'];
+
+    // Recipients
+    $mail->setFrom($_ENV['MAIL_FROM_ADDRESS'], $_ENV['MAIL_FROM_NAME']);
+    $mail->addAddress('recipient@example.com'); // Add a recipient email
+
+    // Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'Test Email';
+    $mail->Body    = 'This is a test email sent using PHPMailer and SMTP.';
+    $mail->AltBody = 'This is the plain text version of the email content.';
+    $mail->addCustomHeader('x-liara-tag', 'test-tag');
+
+
+    // Send the email
+    $mail->send();
+    echo 'Email has been sent successfully!';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
