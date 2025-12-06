154a5e715cb7f06e1288df115637c0acdd212234
Ali Najmabadi <alinajmabadizadeh2002@gmail.com>
2025-12-06 12:38:30 +0330

    add CC and BCC

--- send_email.php
+++ send_email.php
@@ -1,40 +1,44 @@
 <?php
-require 'vendor/autoload.php'; // Load Composer dependencies
+require 'vendor/autoload.php'; 
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 use Dotenv\Dotenv;
 
-// Load environment variables from .env file
 $dotenv = Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
-// Create an instance of PHPMailer
 $mail = new PHPMailer(true);
 
 try {
-    // Server settings
     $mail->isSMTP();
     $mail->Host       = $_ENV['MAIL_HOST'];
     $mail->SMTPAuth   = true;
     $mail->Username   = $_ENV['MAIL_USER'];
     $mail->Password   = $_ENV['MAIL_PASSWORD'];
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS; // Use STARTTLS encryption
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS; 
     $mail->Port       = $_ENV['MAIL_PORT'];
 
-    // Recipients
     $mail->setFrom($_ENV['MAIL_FROM_ADDRESS'], $_ENV['MAIL_FROM_NAME']);
-    $mail->addAddress('recipient@example.com'); // Add a recipient email
+    $mail->addAddress('test@example.com');
 
-    // Content
-    $mail->isHTML(true);                                  // Set email format to HTML
+    // CC:
+    $mail->addCC('test.one@example.com');
+    $mail->addCC('test.two@example.com');
+    $mail->addCC('test.three@example.com');
+
+    // BCC:
+    // $mail->addBCC('test.one@example.com');
+    // $mail->addBCC('test.two@example.com');
+    // $mail->addBCC('test.three@example.com');
+
+    $mail->isHTML(true);                                  
     $mail->Subject = 'Test Email';
     $mail->Body    = 'This is a test email sent using PHPMailer and SMTP.';
     $mail->AltBody = 'This is the plain text version of the email content.';
-    $mail->addCustomHeader('x-liara-tag', 'test-tag'); // use Liara Tags
+    $mail->addCustomHeader('x-liara-tag', 'test-tag'); 
 
 
-    // Send the email
     $mail->send();
     echo 'Email has been sent successfully!';
 } catch (Exception $e) {
